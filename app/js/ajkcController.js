7b70114c061964bd1afe08ab0de54c71da38904a
Chris Phelps <cjphelp@gmail.com>
2013-02-22 22:54:22 -0700

    Display the right icon based on the selected state, click on the icon changes state. Also route and link changes

--- app/js/ajkcController.js
+++ app/js/ajkcController.js
@@ -2,4 +2,8 @@ function AjkcListCtrl($scope, $http) {
     $http.get('data/ajkc59.json').success(function(data) {
         $scope.entries = data;
     });
+
+    $scope.toggleSelected = function(element,state) {
+        $scope.entries[element].selected = state;
+    };
 }
